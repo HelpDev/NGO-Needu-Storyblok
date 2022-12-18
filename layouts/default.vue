e0dc93f085cf3d0f8307cf6eedcb24ab9ca19f0f
Quique Fdez Guerra <quique@ckgrafico.com>
2022-12-18 14:30:51 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Container } from '@papanasi/vue';
+</script>
+
+<template>
+  <Container :fluid="true">
+    <Header />
+    <slot />
+    <Footer />
+  </Container>
+</template>
